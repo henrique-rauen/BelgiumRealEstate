38a62452b8440ff9849570d6ab56d74468c93bec
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-07-05 15:16:47 +0200

    Add rm unwanted by partial and convert columns

--- utils/utils.py
+++ utils/utils.py
@@ -21,7 +21,12 @@ def clean_df(data):
     unwanted_data = ({"Type" : "apartment group"
                      ,"Listing_ID" : -1})
     df = remove_unwanted_data(df, unwanted_data)
-    convert_column = ({int : ["Listing_ID"]
+    unwanted_partial = ({"Postal_code" : " "
+                        ,"Price" : "-"
+                       })
+    df = remove_unwanted_partial(df, unwanted_partial)
+    convert_column = ({int : ["Listing_ID", "Bedroom", "Living_area",
+                      "Surface_of_land", "Facade", "Garden_area"]
                     })
     df = convert_column_type(df,convert_column)
     return df
@@ -30,17 +35,26 @@ def fill_NaN(df,dic_defaults={}):
     for key,value in dic_defaults.items():
         for column in value:
             df[column]=df[column].fillna(key)
+    print("Filled NaN")
+    return df
+
+def remove_unwanted_partial(df,dic_partial):
+    for key,value in dic_partial.items():
+        df = df[~df[key].str.contains(value)]
+    print("Removed partial unwanted")
     return df
 
 def remove_unwanted_data(df,dic_unwanted):
     for key,value in dic_unwanted.items():
         df = df[df[key] != value]
+    print("Removed unwanted")
     return df
 
 def convert_column_type(df,dic_convert):
     for key,value in dic_convert.items():
         for column in value:
             df[column] = df[column].apply(key)
+    print("Converted Columns")
     return df
 
 def remove_empty_spaces(df):
