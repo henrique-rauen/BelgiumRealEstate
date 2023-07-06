62e2356cb6974546e4f76c1e2b5aeaf64a274fed
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-07-06 10:30:40 +0200

    Plots

--- utils/plots.py
+++ utils/plots.py
@@ -5,12 +5,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def plot(df):
-    print("trying to plot")
     #sns.relplot(data=df[df["Living_area"] > 50], y="Living_area", x="Price",kind="scatter", hue="Type")
 #    sns.scatterplot(data=df[(df["Living_area"] > 50) & (df["Price"] < 1000000) &
 #                 (df["Price"] > 80000)], x="Price", y="Living_area",hue="Type")
-    data = df.loc[:, ["Locality", "Price"]]
-    data = data.groupby("Locality",as_index=False).mean().sort_values(by="Price")
+    #data = df.loc[:, ["Locality", "Price"]]
+    data = df
+    #data = data.groupby("Locality",as_index=False).Price.agg(["mean","count"])
+    #data = data[data["count"]>10].sort_values("count")
     print(data)
-    #sns.relplot(data=data,x="Locality", y="Price")
+    sns.histplot(data=data,x="Bedroom")
     plt.show()
