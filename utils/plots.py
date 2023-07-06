3f3d85789a352238b21f01ea6a6e02c32a97e2a0
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-07-06 15:19:13 +0200

    Plot

--- utils/plots.py
+++ utils/plots.py
@@ -14,15 +14,18 @@ def plot(df):
     #data = data[data["count"]>10].sort_values("count")
     #print(data)
     data = data[(data["Living_area"] > 0) & (data["Living_area"] < 12000)]
+    data = data[data["Price"] < 5000000]
     #print(data.groupby("Type", as_index=False).agg({"Living_area" :
     #                                                ["mean"]}))
     #sns.histplot(data=data,x="Living_area",hue="Type")
-    data = data.groupby("Postal_code", as_index=False).agg({"Price": ["mean",
-                                                                      "count"]
-                                                            ,"lng" : ["max"]
-                                                            ,"lat" : ["max"]
-                                                           })
-    #data[("Price", "count")] = data[("Price", "count")]*10
-    sns.scatterplot(data=data,x=("lng", "max"),y=("lat", "max"),
-                    size=("Price", "count"), sizes=(100,10000))
+    #data = data.groupby("city", as_index=False).agg({"Price": ["mean",
+    #                                                                  "count"]
+    #                                                        ,"lng" : ["max"]
+    #                                                        ,"lat" : ["max"]
+    #                                                       })
+    #data=data[data[("Price", "count")]>5]
+    #sns.scatterplot(data=data,x=("lng", "max"),y=("lat", "max"),
+    #                size=("Price", "mean"), sizes=(10,600))
+    data["city"]= data["city"].astype('category').cat.codes
+    sns.scatterplot(data=data, x="city", y="Price")
     plt.show()
