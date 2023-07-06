05aeb14e1829764e45f4e95e7406eb3f47614a3b
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-07-06 17:11:38 +0200

    Correct correlation ratio func

--- utils/stats.py
+++ utils/stats.py
@@ -2,15 +2,16 @@
 
 #Created by Henrique Rauen (rickgithub@hsj.email)
 import numpy as np
+
 def correlation_ratio(categories, values):
     categories = np.array(categories)
     values = np.array(values)
 
     ss_category = 0
-    ss_total = 0
+    ss_outside = 0
     for category in set(categories): #Iterate only through distinct categories
         subgroup = values[np.where(categories == category)[0]]
         ss_category += sum((subgroup-np.mean(subgroup))**2)
-        ss_total += len(subgroup)*(np.mean(subgroup)-np.mean(values))**2
+        ss_outside += len(subgroup)*(np.mean(subgroup)-np.mean(values))**2
 
-    return (ss_total / (ss_total + ss_category))**.5
+    return (ss_outside / (ss_total + ss_category))**.5
