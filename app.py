0c9a89035b48eac91610e0120ea95df7ef6eb4d2
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-07-27 15:32:32 +0200

    Add docker file, app works well

--- app.py
+++ app.py
@@ -3,11 +3,37 @@
 #Created by Henrique Rauen (rickgithub@hsj.email)
 from src import model
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-dt = pd.read_csv("teste_data.csv")
-my_model = model.Model()
-#dt = dt[dt["Open Fire"]==True]
-dt = dt.head(6060)
-result = my_model.apply_model(dt)
-print(result[["District", "Subtype", "Living_area","Open Fire","Surface_of_land","Price","Predictions"]])
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+#@app.get("/model")
+#def get():
+#
+#    return msg
+
+@app.post("/get_prediction/")
+def get_prediction(payload : dict = Body()):
+    mandatory_fields = ["Living_area"
+                        ,"Type"
+                        ,"District"
+                        ,"Subtype"
+                        ,"Open Fire"
+                        ,"Surface_of_land"
+                       ]
+    if not all(v in payload.keys() for v in mandatory_fields):
+        return ({'result' : 'Not all required fields were added, make sure the input is'
+                + ' correct'})
+    my_model = model.Model()
+    data = pd.DataFrame.from_dict(payload)
+    data["Type"] = data["Type"].fillna("house")
+    data["Living_area"] = data["Living_area"].fillna(0)
+    result = my_model.apply_model(data)
+    return result.to_json()
+
+#dt = pd.read_csv("teste_data.csv")
+#my_model = model.Model()
+##dt = dt[dt["Open Fire"]==True]
+#dt = dt.head(6060)
+#result = my_model.apply_model(dt)
+#print(result[["District", "Subtype", "Living_area","Open Fire","Surface_of_land","Price","Predictions"]])
